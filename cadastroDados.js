f90b3ac86a0cb9733f46f93f8721b07ddc2f347b
Henrique Ferraz <henriqueoliver9@gmail.com>
2022-01-11 18:25:25 -0300

    terminando por hoje

--- cadastroDados.js
+++ cadastroDados.js
@@ -7,19 +7,19 @@ const email = document.getElementById("email")
 const labelSenha = document.getElementById("labelSemha")
 const senha = document.getElementById("senha")
 const LabelCorfirmarSenha = document.getElementById("labelConfirmarSenha")
-const confirmarSenha= document.getElementById("confirmarSenha")
+const confirmarSenha = document.getElementById("confirmarSenha")
 const btnConfirmar = document.getElementById("confirmar")
 
 nomeDOM.addEventListener('keyup', () => {
-    if(nomeDOM.value.length <= 2 || nomeDOM.value == ""){
+    if(nomeDOM.value.length <= 2 || nomeDOM.value === " "){
         nomeDOM.setAttribute('style', 'border-color: red')
     }else{
         nomeDOM.setAttribute('style', 'border-color : green')
     }
 })
 sobreNomeDOM.addEventListener('keyup', () => {
-    if(sobreNomeDOM.value.length <= 3 || sobreNomeDOM.value == ""){
-        nomeDOM.setAttribute('style', 'border-color: red')
+    if(sobreNomeDOM.value.length <= 3 || sobreNomeDOM.value === " "){
+        sobreNomeDOM.setAttribute('style', 'border-color: red')
     }else{
         sobreNomeDOM.setAttribute('style', 'border-color : green')
     }
